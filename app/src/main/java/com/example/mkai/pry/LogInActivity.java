e040f7e9426fddfcea65eae1303ea8c82cb2e34b
SAmser93 <samser_krut93@mail.ru>
2016-06-16 15:42:04 +0300

    Update LogInActivity.java

--- app/src/main/java/com/example/mkai/pry/LogInActivity.java
+++ app/src/main/java/com/example/mkai/pry/LogInActivity.java
@@ -17,9 +17,44 @@ public class LogInActivity extends Activity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
     }
+    
+    /**
+     * Проверка на наличие подключения к Интернету
+     */
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+    
+     /**
+     * Вывод местного MessageBox
+     * @title - текст, что пойдёт в заглавие
+     * @text - текст самого сообщения
+     */
+    public void showMsgBox(String title,String text)
+    {
+        AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
+        dlgAlert.setTitle(title);
+        dlgAlert.setMessage(text);
+        dlgAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                //finish();
+            }
+        });
+        dlgAlert.setCancelable(true);
+        dlgAlert.create().show();
+    }
 
     @Override
     public void onClick(View v) {
+        /*Вот так можно вызвать проверку соединения. По ходу склейки будет видно, где его стоит применить
+        if (!isOnline())
+        {
+            showMsgBox("Error","No Internet connection");
+            return;
+        }*/
         switch (v.getId()) {
             case R.id.btn_login:
                 //Call SUbscriptionActivity
